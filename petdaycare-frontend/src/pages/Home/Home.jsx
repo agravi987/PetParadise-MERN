f41ca8d4847ec152310189d10e778189b43b4f97
Ravi Agrahari <agpashupati@gmail.com>
2025-04-03 15:31:23 +0530

    remove material ui

--- petdaycare-frontend/src/pages/Home/Home.jsx
+++ petdaycare-frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,3 @@
-import { Button, Typography, Box } from "@mui/material";
-import Navbar from "../../components/Navbar";
 import Navbar from "../../components/Navbar";
 import Header from "../../components/Header";
 
@@ -7,18 +5,6 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <h1 class="text-3xl font-bold underline">Hello Friends!</h1>
-     
-      <Typography variant="h3" gutterBottom>
-        🐶 PetDaycare
-      </Typography>
-      <Typography variant="h5" sx={{ mb: 4 }}>
-        Book daycare for your pets!
-      </Typography>
-      <Button variant="contained" href="/login" size="large">
-        Get Started
-      </Button>
-    </Box>
       <Header />
     </>
   );
